364a2f8800603410a6810a772a321d0840f636f5
Sharran <sharran.murali@gmail.com>
2025-01-13 10:28:13 +0530

    Initial commit to move all my solved challenges from "msharran/labs" repo to here.

---
+++ sort/rust-sort/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let seconds = Seconds(60);
+    let minutes = Minutes(1);
+    // let seconds = seconds.into(minutes);
+    // println!("{} seconds", seconds.0);
+}
+
+struct Seconds(u32);
+
+struct Minutes(u32);
+
+impl Fr 
