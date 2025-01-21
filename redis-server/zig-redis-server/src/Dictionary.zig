7a731dc538ca966340c118282d20ea6a3a5048f0
Sharran <sharran.murali@gmail.com>
2025-01-21 17:21:26 +0530

    zig-redis server: RwLock for dictionary access

--- redis-server/zig-redis-server/src/Dictionary.zig
+++ redis-server/zig-redis-server/src/Dictionary.zig
@@ -1,10 +1,13 @@
 const std = @import("std");
 const log = std.log.scoped(.dictionary);
 const Dictionary = @This();
+const RwLock = std.Thread.RwLock;
 
 entries: std.StringHashMap(Value),
 
 allocator: std.mem.Allocator,
+
+rwlock: *RwLock,
 
 /// A tagged union that represents a Redis object.
 /// Tagged unions seems to be a good fit for this kind of data.
@@ -18,32 +21,47 @@ pub const ValueType = enum {
 };
 
 pub fn init(allocator: std.mem.Allocator) !*Dictionary {
+    const rwlock = try allocator.create(RwLock);
+    rwlock.* = .{};
+
     const dict = Dictionary{
         .entries = std.StringHashMap(Value).init(allocator),
         .allocator = allocator,
+        .rwlock = rwlock,
     };
+
     const dict_p = try allocator.create(Dictionary);
     dict_p.* = dict;
+
     return dict_p;
 }
 
 pub fn deinit(self: *Dictionary) void {
+    self.allocator.destroy(self.rwlock);
+
     var iter = self.entries.iterator();
     while (iter.next()) |kv| {
         const key_p = kv.key_ptr.*;
         self.allocator.free(key_p);
     }
     self.entries.deinit();
+
     self.allocator.destroy(self);
 }
 
 pub fn putString(self: *Dictionary, key: []const u8, value: []const u8) !void {
+    self.rwlock.lock();
+    defer self.rwlock.unlock();
+
     const key_p = try self.allocator.dupe(u8, key);
     const val = Value{ .string = value };
     try self.entries.put(key_p, val);
 }
 
 pub fn getString(self: *Dictionary, key: []const u8) !?[]const u8 {
+    self.rwlock.lockShared();
+    defer self.rwlock.unlockShared();
+
     const val = self.entries.get(key);
 
     if (val) |v| return v.string;
@@ -51,6 +69,9 @@ pub fn getString(self: *Dictionary, key: []const u8) !?[]const u8 {
 }
 
 pub fn printAll(self: Dictionary) void {
+    self.rwlock.lockShared();
+    defer self.rwlock.unlockShared();
+
     var iter = self.entries.iterator();
     while (iter.next()) |kv| {
         const k = kv.key_ptr.*;
