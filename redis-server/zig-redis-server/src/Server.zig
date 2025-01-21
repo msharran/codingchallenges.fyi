d3e4c11af5c4b3adb146045373971f9d1dd5b88c
Sharran <sharran.murali@gmail.com>
2025-01-21 13:54:43 +0530

    move handling connection as separate function. This would make it easy to use it with threads

--- redis-server/zig-redis-server/src/Server.zig
+++ redis-server/zig-redis-server/src/Server.zig
@@ -1,14 +1,14 @@
 const std = @import("std");
 const log = std.log.scoped(.server);
-
-const Dictionary = @import("Dictionary.zig");
-const Router = @import("Router.zig");
-const Server = @This();
-
 const net = std.net;
 const posix = std.posix;
+
+const Server = @This();
+const Dictionary = @import("Dictionary.zig");
+const Router = @import("Router.zig");
 const Resp = @import("Resp.zig");
 const Request = @import("command.zig").Request;
+const ThreadPool = std.Thread.Pool;
 
 /// router is responsible for handling the commands received by the server.
 router: Router,
@@ -52,50 +52,54 @@ pub fn listenAndServe(self: *Server, address: std.net.Address) !void {
 
     // TODO: Implement ring buffer for reading and writing
     // https://discord.com/channels/1085221098129473637/1179194569800294550/1329122172685516860
-    var buf: [128]u8 = undefined;
     while (true) {
         var client_address: net.Address = undefined;
         var client_address_len: posix.socklen_t = @sizeOf(net.Address);
 
-        const socket = posix.accept(listener, &client_address.any, &client_address_len, 0) catch |err| {
+        const connection = posix.accept(listener, &client_address.any, &client_address_len, 0) catch |err| {
             log.err("Failed to accept connection: {}", .{err});
-            continue;
-        };
-        defer posix.close(socket);
-
-        log.info("Client connected: {}", .{client_address});
-        defer log.info("Client connection closed: {}", .{client_address});
-
-        const read = posix.read(socket, &buf) catch |err| {
-            log.err("Failed to read from client: {}", .{err});
-            continue;
+            return;
         };
 
-        if (read == 0) {
-            log.info("Read 0 bytes from client, closing connection", .{});
-            continue;
-        }
-
-        const resp = Resp.init(self.allocator);
-        defer resp.deinit();
-
-        const msg = resp.deserialise(buf[0..read]) catch |err| {
-            log.err("Failed to deserialise message: {}", .{err});
-            continue;
-        };
-
-        const req = Request{ .message = msg, .dict = self.dict };
-        const resp_msg = try self.router.route(req);
-
-        const raw_msg = resp.serialise(resp_msg) catch |err| {
-            log.err("Failed to serialise message: {}", .{err});
-            continue;
-        };
-
-        writeAll(socket, raw_msg) catch |err| {
-            log.err("Failed to write to client: {}", .{err});
-        };
+        self.handle_connection(connection, client_address);
     }
+}
+
+fn handle_connection(self: *Server, connection: posix.socket_t, client_addr: net.Address) void {
+    defer posix.close(connection);
+    log.info("Client connected: {}", .{client_addr});
+    defer log.info("Client connection closed: {}", .{client_addr});
+
+    var buf: [128]u8 = undefined;
+    const read = posix.read(connection, &buf) catch |err| {
+        log.err("Failed to read from client: {}", .{err});
+        return;
+    };
+
+    if (read == 0) {
+        log.info("Read 0 bytes from client, closing connection", .{});
+        return;
+    }
+
+    const resp = Resp.init(self.allocator);
+    defer resp.deinit();
+
+    const msg = resp.deserialise(buf[0..read]) catch |err| {
+        log.err("Failed to deserialise message: {}", .{err});
+        return;
+    };
+
+    const req = Request{ .message = msg, .dict = self.dict };
+    const resp_msg = try self.router.route(req);
+
+    const raw_msg = resp.serialise(resp_msg) catch |err| {
+        log.err("Failed to serialise message: {}", .{err});
+        return;
+    };
+
+    writeAll(connection, raw_msg) catch |err| {
+        log.err("Failed to write to client: {}", .{err});
+    };
 }
 
 fn writeAll(socket: posix.socket_t, msg: []const u8) !void {
