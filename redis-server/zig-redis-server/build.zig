efe1dc32392322e8d933a2ddfc4961b34c0ada7a
Sharran <sharran.murali@gmail.com>
2025-01-24 19:00:49 +0530

    Added libxev as deps

--- redis-server/zig-redis-server/build.zig
+++ redis-server/zig-redis-server/build.zig
@@ -6,12 +6,17 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
+
     const exe = b.addExecutable(.{
         .name = "redis-server",
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
+
+    const xev = b.dependency("libxev", .{ .target = target, .optimize = optimize });
+    exe.root_module.addImport("xev", xev.module("xev"));
+
     b.installArtifact(exe);
 
     const run_cmd = b.addRunArtifact(exe);
