417540534d150de841247a7da947691c41b48ed9
Sharran <sharran.murali@gmail.com>
2025-08-26 20:05:24 +0530

    zig-strace: extract fn for child and parent executions

--- strace/zig-strace/src/main.zig
+++ strace/zig-strace/src/main.zig
@@ -8,6 +8,44 @@ const stdout = std.io.getStdOut().writer();
 
 pub const log_level: std.log.Level = .debug;
 
+fn continueChild(allocator: std.mem.Allocator, args: [][:0]u8) !void {
+    // Child process will return pid 0
+
+    // PTRACE_TRACEME: Child declares itself traceable by parent
+    // see: https://man7.org/linux/man-pages/man2/ptrace.2.html#:~:text=to%20be%20performed%3A-,PTRACE_TRACEME,-Indicate%20that%20this
+    try posix.ptrace(linux.PTRACE.TRACEME, 0, 0, 0);
+
+    const child_cmd = args[1..];
+    const argv = try allocator.alloc(?[*:0]const u8, child_cmd.len + 1);
+    defer allocator.free(argv);
+
+    for (args[1..], 0..) |arg, i| {
+        argv[i] = arg;
+    }
+    argv[child_cmd.len] = null;
+
+    var i: usize = 0;
+    while (argv[i] != null) : (i += 1) {
+        // we should unwrap optional pointer
+        log.debug("argv[{d}]: {s}", .{ i, argv[i].? });
+    }
+
+    const envp = [_:null]?[*:0]const u8{null};
+    const argv_ptr: [*:null]const ?[*:0]const u8 = @ptrCast(argv.ptr);
+
+    // execvpeZ: The 'Z' suffix indicates null-terminated strings (C-style)
+    // - file: null-terminated string ([*:0]const u8)
+    // - argv: array of null-terminated string pointers
+    // - envp: array of null-terminated string pointers
+    return posix.execvpeZ(args[1], argv_ptr, &envp);
+}
+
+fn waitChildAndContinueParent(pid: posix.pid_t) void {
+    // Parent process will recieve child's pid
+    const res = posix.waitpid(pid, 0);
+    log.debug("child result: {}", .{res});
+}
+
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer {
@@ -24,33 +62,17 @@ pub fn main() !void {
         return;
     }
 
-    const child_cmd = args[1..]; // Include command name for execve
-
     const pid = try posix.fork();
 
     if (pid == 0) {
-        // Child process will return pid 0
-        const argv = try allocator.alloc(?[*:0]const u8, child_cmd.len + 1);
-        defer allocator.free(argv);
-
-        for (args[1..], 0..) |arg, i| {
-            argv[i] = arg;
-        }
-        argv[child_cmd.len] = null;
-
-        var i: usize = 0;
-        while (argv[i] != null) : (i += 1) {
-            // we should unwrap optional pointer
-            log.debug("argv[{d}]: {s}", .{ i, argv[i].? });
-        }
-
-        const envp = [_:null]?[*:0]const u8{null};
-        const argv_ptr: [*:null]const ?[*:0]const u8 = @ptrCast(argv.ptr);
-
-        return posix.execvpeZ(args[1], argv_ptr, &envp);
+        return continueChild(allocator, args);
     } else {
-        // Parent process will recieve child's pid
-        const res = posix.waitpid(pid, 0);
-        log.debug("child result: {}", .{res});
+        waitChildAndContinueParent(pid);
     }
+}
+
+test "ptrace function signature" {
+    // Test to understand ptrace signature with PTRACE_ATTACH
+    const result = posix.ptrace(linux.PTRACE.ATTACH, 1, null, null);
+    _ = result;
 }
