392fcc1e9d56db812d472d9fec1163a8c137896b
Sharran <sharran.murali@gmail.com>
2025-08-27 14:05:13 +0530

    zig-strace: implement PTRACE_TRACEME, PTRACE_SYSCALL and PTRACE_DETACH to mimic strace behaviour. It is a basic implementation

--- strace/zig-strace/src/main.zig
+++ strace/zig-strace/src/main.zig
@@ -2,8 +2,8 @@ const std = @import("std");
 const os = std.os;
 const linux = os.linux;
 const posix = std.posix;
-const eprint = std.debug.print;
-const log = std.log.scoped(.strace);
+const log_tracer = std.log.scoped(.tracer);
+const log_tracee = std.log.scoped(.tracee);
 const stdout = std.io.getStdOut().writer();
 
 pub const log_level: std.log.Level = .debug;
@@ -11,9 +11,13 @@ pub const log_level: std.log.Level = .debug;
 fn continueChild(allocator: std.mem.Allocator, args: [][:0]u8) !void {
     // Child process will return pid 0
 
-    // PTRACE_TRACEME: Child declares itself traceable by parent
+    log_tracee.debug("traceme! stopping execution",.{});
+    // Child declares itself traceable by parent
     // see: https://man7.org/linux/man-pages/man2/ptrace.2.html#:~:text=to%20be%20performed%3A-,PTRACE_TRACEME,-Indicate%20that%20this
     try posix.ptrace(linux.PTRACE.TRACEME, 0, 0, 0);
+    // common practice to call this as per manual
+    // ref: https://man7.org/linux/man-pages/man2/ptrace.2.html#:~:text=A%20common%20practice%20is%20to%20follow%0A%20%20%20%20%20%20%20the%20PTRACE_TRACEME%20with%0A%0A%20%20%20%20%20%20%20%20%20%20%20raise(SIGSTOP)%3B
+    try posix.raise(linux.SIG.STOP);
 
     const child_cmd = args[1..];
     const argv = try allocator.alloc(?[*:0]const u8, child_cmd.len + 1);
@@ -27,7 +31,7 @@ fn continueChild(allocator: std.mem.Allocator, args: [][:0]u8) !void {
     var i: usize = 0;
     while (argv[i] != null) : (i += 1) {
         // we should unwrap optional pointer
-        log.debug("argv[{d}]: {s}", .{ i, argv[i].? });
+        log_tracee.debug("prepare exec -> argv[{d}]: {s}", .{ i, argv[i].? });
     }
 
     const envp = [_:null]?[*:0]const u8{null};
@@ -40,17 +44,40 @@ fn continueChild(allocator: std.mem.Allocator, args: [][:0]u8) !void {
     return posix.execvpeZ(args[1], argv_ptr, &envp);
 }
 
-fn waitChildAndContinueParent(pid: posix.pid_t) void {
-    // Parent process will recieve child's pid
-    const res = posix.waitpid(pid, 0);
-    log.debug("child result: {}", .{res});
+fn waitChildAndContinueParent(pid: posix.pid_t) !void {
+    // Child used PTRACE_TRACEME and raise(SIGSTOP),
+    // so wait for the child to stop
+    log_tracer.debug("expecting SIGSTOP from child {}", .{pid});
+    var res = posix.waitpid(pid, 0);
+    log_tracer.debug("child wait {} WIFEXITED={} WIFSTOPPED={}",.{res, linux.W.IFEXITED(res.status), linux.W.IFSTOPPED(res.status)});
+
+    log_tracer.debug("expected child to stop at syscall-start {}", .{pid});
+    try posix.ptrace(linux.PTRACE.SYSCALL, pid, 0, 0);
+    res = posix.waitpid(pid, 0);
+    log_tracer.debug("child wait {} WIFEXITED={} WIFSTOPPED={}",.{res, linux.W.IFEXITED(res.status), linux.W.IFSTOPPED(res.status)});
+
+    log_tracer.debug("expected child to stop at syscall-end {}", .{pid});
+    try posix.ptrace(linux.PTRACE.SYSCALL, pid, 0, 0);
+    res = posix.waitpid(pid, 0);
+    log_tracer.debug("child wait {} WIFEXITED={} WIFSTOPPED={}",.{res, linux.W.IFEXITED(res.status), linux.W.IFSTOPPED(res.status)});
+
+    // try posix.ptrace(linux.PTRACE.SYSCALL, pid, 0, 0);
+    // res = posix.waitpid(pid, 0);
+    // log.debug("child WIFEXITED={} WIFSTOPPED={}",.{linux.W.IFEXITED(res.status), linux.W.IFSTOPPED(res.status)});
+    // log.debug("child stopped: {}", .{res});
+
+    log_tracer.debug("resuming child {}",.{pid});
+    try posix.ptrace(linux.PTRACE.DETACH, pid, 0, 0);
+    res = posix.waitpid(pid, 0);
+    log_tracer.debug("child wait {} WIFEXITED={} WIFSTOPPED={}",.{res, linux.W.IFEXITED(res.status), linux.W.IFSTOPPED(res.status)});
+
 }
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer {
         const check = gpa.deinit();
-        log.debug("Mem leak check: {}", .{check});
+        log_tracer.debug("Mem leak check: {}", .{check});
     }
     const allocator = gpa.allocator();
 
@@ -58,16 +85,17 @@ pub fn main() !void {
     defer std.process.argsFree(allocator, args);
 
     if (args.len < 2) {
-        eprint("Usage: {s} <command> [args...]\n", .{args[0]});
+        log_tracer.err("Usage: {s} <command> [args...]", .{args[0]});
         return;
     }
 
+    log_tracer.debug("forking process", .{});
     const pid = try posix.fork();
 
     if (pid == 0) {
         return continueChild(allocator, args);
     } else {
-        waitChildAndContinueParent(pid);
+        try waitChildAndContinueParent(pid);
     }
 }
 
